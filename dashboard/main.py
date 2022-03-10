ca8227cc9fafef76bb58203b8acbc464891d9faf
Federico Torres <federicotorres2109@gmail.com>
2022-03-10 10:54:38 -0600

    fix:Readme.md

--- dashboard/main.py
+++ dashboard/main.py
@@ -15,7 +15,7 @@ if page == 'Information':
     st.text("that is capable of recognizing the different sounds in an environment.")
     st.subheader("Quick explanation")
 
-    st.image(r"./images/diagram.png")
+    st.image(r"dashboard/images/diagram.png")
 
 
 
